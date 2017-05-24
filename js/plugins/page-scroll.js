9f905d442b480876618d8b2e635d874e92bc5a85
marlmarl <marlenape@gmail.com>
2017-05-24 20:19:13 +0200

    about pop up redesign - just tryin'

--- js/plugins/page-scroll.js
+++ js/plugins/page-scroll.js
@@ -1,9 +1,9 @@
-    /*PAGE SCROLL from Agency Start Bootstrap Template*/
+    /*PAGE SCROLL from Agency Start Bootstrap Template - modified*/
 
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 1)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
